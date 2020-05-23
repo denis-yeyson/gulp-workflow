699bd22607688af1fa343dcc16a14c8a7b64e7a7
DENIS YEYSÓN ESPINOZA PONCIANO <denisyeyson@gmail.com>
2020-05-23 18:36:12 -0500

    Actualizacion del archivo crear_directorio.js con expresiones regulares

--- crear_directorio.js
+++ crear_directorio.js
@@ -2,71 +2,86 @@
 //                       CREAR DIRECTORIO DE FICHEROS Y ARCHIVOS                       //
 //.....................................................................................//
 
-const shell = require('shelljs');
-const fs = require('fs');
+let fs = require("fs");
+let shell = require("shelljs");
 
-function crearFolders(cadena) {
-    if (cadena.indexOf('{') > 0) {
-        let inicio = cadena.indexOf('{') + 1;
-        let fin = cadena.indexOf('}');
-        let carpetas = cadena.substring(0, inicio - 1);
-        let subcarpetas = cadena.substring(inicio, fin).split(',');
-        for (cp in subcarpetas) {
-            subcarpetas[cp] = carpetas + '/' + subcarpetas[cp];
-        }
-        shell.mkdir('-p', carpetas, subcarpetas);
-    } else {
-        shell.mkdir('-p', cadena);
-    }
+const FILES = "files";
+const FOLDERS = "folders";
+
+/**
+ *
+ * @param {*} tipo Colocar el tipo de elemento a crear "folders" o "files"
+ * @param {*} cadena Parametro ejemplo elemento_1/elemento_2{subelemento_1.txt,subelemento_2.txt}
+ */
+function crear(tipo, cadena) {
+  let elemento = "";
+  if (cadena.indexOf("{") > 0) {
+    elemento = cadena.match(/(.*){/)[1];
+    let subelemento = cadena.match(/{(.*)}/)[1].split(",");
+    for (cp in subelemento) subelemento[cp] = elemento + "/" + subelemento[cp];
+    cadena = subelemento;
+  }
+  if (tipo === FILES) {
+    shell.mkdir("-p", elemento);
+    shell.touch(cadena);
+  } else {
+    shell.mkdir("-p", cadena);
+  }
 }
 
-function crearArchivo(name) {
-    if (name.indexOf('{') > 0) {
-        let inicio = name.indexOf('{') + 1;
-        let fin = name.indexOf('}');
-        let carpetas = name.substring(0, inicio - 1);
-        let archivos = name.substring(inicio, fin).split(',');
-        for (cp in archivos) {
-            shell.touch(carpetas + '/' + archivos[cp]);
-        }
-    } else {
-        shell.touch(name);
-    }
+function crearFolders(cadena) {
+  crear(FOLDERS, cadena);
+}
+
+function crearArchivos(cadena) {
+  crear(FILES, cadena);
 }
 
 var plantilla_main_scss = `@charset "UTF-8";\n\n/*! denis-yeyson ;*/\n\n// Settings\n@import "settings/colors";\n@import "settings/global";\n\n// Tools\n@import "tools/mixins";\n\n// Generic\n@import "generic/normalize";\n@import "generic/box-sizing";\n@import "generic/reset";\n@import "generic/shared";\n\n// Elements\n@import "elements/page";\n@import "elements/headings";\n@import "elements/links";\n@import "elements/lists";\n@import "elements/images";\n@import "elements/quotes";\n\n// Objects\n@import "objects/wrappers";\n@import "objects/layout";\n@import "objects/media";\n\n// Components\n@import "components/page-title";\n@import "components/buttons";\n@import "components/testimonials";\n@import "components/avatars";\n\n// Trumps\n@import "trumps/links";\n@import "trumps/widths";`;
 
-
 function crearDirectorio() {
-    let path = './src/assets/';
+  let path = "./src/assets/";
 
-    //CARPETAS DE IMAGENES
-    console.log('CREANDO CARPETAS DE IMAGENES...');
-    crearFolders(path + 'img{jpg,png,svg}');
+  //CARPETAS DE IMAGENES
+  console.log("CREANDO CARPETAS DE IMAGENES...");
+  crearFolders(path + "img{jpg,png,svg}");
 
-    //CARPETAS DE SCSS
-    console.log('CREANDO CARPETA Y ARCHIVOS DE SCSS CON NOMENCLATURA \'ITCSS\'...');
-    crearFolders(path + 'scss{settings,tools,generic,elements,objects,components,trumps}');
-    crearArchivo(path + 'scss/settings{_colors.scss,_global.scss}');
-    crearArchivo(path + 'scss/tools{_mixins.scss}');
-    crearArchivo(path + 'scss/generic{_normalize.scss,_box-sizing.scss,_reset.scss,_shared.scss}');
-    crearArchivo(path + 'scss/elements{_page.scss,_headings.scss,_links.scss,_lists.scss,_images.scss,_quotes.scss}');
-    crearArchivo(path + 'scss/objects{_wrappers.scss,_layout.scss,_media.scss}');
-    crearArchivo(path + 'scss/components{_page-title.scss,_buttons.scss,_testimonials.scss,_avatars.scss}');
-    crearArchivo(path + 'scss/trumps{_links.scss,_widths.scss}');
-    crearArchivo(path + 'scss/main.scss');
-    fs.writeFile(path + 'scss/main.scss', plantilla_main_scss, () => {});
+  //CARPETAS DE SCSS CON ARQUITECTURA ITCSS Y BEM (BEMIT)
+  console.log("CREANDO CARPETA Y ARCHIVOS DE SCSS CON NOMENCLATURA 'ITCSS'...");
+  crearFolders(
+    path + "scss{settings,tools,generic,elements,objects,components,trumps}"
+  );
+  crearArchivos(path + "scss/settings{_colors.scss,_global.scss}");
+  crearArchivos(path + "scss/tools{_mixins.scss}");
+  crearArchivos(
+    path +
+      "scss/generic{_normalize.scss,_box-sizing.scss,_reset.scss,_shared.scss}"
+  );
+  crearArchivos(
+    path +
+      "scss/elements{_page.scss,_headings.scss,_links.scss,_lists.scss,_images.scss,_quotes.scss}"
+  );
+  crearArchivos(path + "scss/objects{_wrappers.scss,_layout.scss,_media.scss}");
+  crearArchivos(
+    path +
+      "scss/components{_page-title.scss,_buttons.scss,_testimonials.scss,_avatars.scss}"
+  );
+  crearArchivos(path + "scss/trumps{_links.scss,_widths.scss}");
+  crearArchivos(path + "scss/main.scss");
+  fs.writeFile(path + "scss/main.scss", plantilla_main_scss, () => {});
 
-    //CARPETAS DE JAVASCRIPT
-    console.log('CREANDO CARPETA Y ARCHIVOS DE JAVASCRIPT...');
-    crearFolders(path + 'js');
-    crearArchivo(path + 'js/script.js');
+  //CARPETAS DE JAVASCRIPT
+  console.log("CREANDO CARPETA Y ARCHIVOS DE JAVASCRIPT...");
+  crearFolders(path + "js");
+  crearArchivos(path + "js/script.js");
 
-    //CARPETAS DE PUG
-    console.log('CREANDO CARPETA Y ARCHIVOS DE PUG...');
-    crearFolders('./src/pug{pages,tools}');
-    crearArchivo('./src/pug/tools{config_variables.pug,include_head.pug,include_footer.pug,mixin_funciones.pug,template_index.pug}');
-    crearArchivo('./src/pug/index.pug');
+  //CARPETAS DE PUG
+  console.log("CREANDO CARPETA Y ARCHIVOS DE PUG...");
+  crearFolders("./src/pug{pages,tools}");
+  crearArchivos(
+    "./src/pug/tools{config_variables.pug,include_head.pug,include_footer.pug,mixin_funciones.pug,template_index.pug}"
+  );
+  crearArchivos("./src/pug/index.pug");
 }
 
 crearDirectorio();
